aa3905540127046241783d61eed078c928a10949
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-03-06 15:41:11 -0300

    switch case

---
+++ exercicios/switch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+    {
+        int nivel = 1;
+        switch(nivel)
+        {
+            case 1: 
+                puts("Iniciante!");
+                break;
+            case 2: 
+                puts("Experiente");
+                break;
+        }
+        return 1;
+    }
